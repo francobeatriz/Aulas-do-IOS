f9674a82cd22da66a33a00f7c192524b250ae515
Beatriz Franco <129182039+francobeatriz@users.noreply.github.com>
2023-05-04 11:12:28 -0300

    Add files via upload

---
+++ aula03.js
@@ -0,0 +1,35 @@
+// var nome = "Bia"
+// var media = 8; 
+// var pontoBonus = 2
+// console.log(media);
+
+// media += 1; //incremento media = media + pontoBonus
+
+// console.log(media);
+
+// console.log(typeof media);
+// console.log (typeof nome);
+
+// // NaN
+// var num1 = Infinity
+// console.log(typeof num1)
+
+// var nome = "Bia"; // Escopo global 
+// let idade = 15; // não da pra redeclarar mas da pra trocar o valor
+// const PI = 3.14 // valor constante 
+
+
+EXERCICIO:
+
+var nome = "Bia";
+let idade = 21;
+const tenhoDinheiro = true
+
+console.log (`Meu nome é ${nome} e eu tenho ${idade} anos, dizem que eu nunca tenho dinheiro, isso é verdade? ${tenhoDinheiro}`);
+
+idade = "18";
+var nome = "Bia";
+
+
+console.log (`Meu nome é ${nome} e eu tenho ${idade} anos, dizem que eu nunca tenho dinheiro, isso é verdade? ${tenhoDinheiro}`);
+
---
+++ aula03pt2.js
@@ -0,0 +1,13 @@
+// alert("Olá Mundo!"); // window não é necessário, funciona sem 
+// let nome = prompt ("Qual o seu nome?"); // receber uma informação do usuário
+
+// alert(`Olá, ${nome}, tudo bem? Vamos fazer uma soma!`);
+
+// let num1 = Number(prompt('Digite um número '));
+// let num2 = Number(prompt('Digite outro número '));
+
+// let ress = num1 + num2; // Number ou parseFloat
+
+// alert(`A soma dos valores ${num1} e ${num2} é: ${res}`);
+
+console.log()
