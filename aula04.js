3774978afd22bf3f8496276d36bf5888e54a4131
Beatriz Franco <129182039+francobeatriz@users.noreply.github.com>
2023-05-08 11:59:48 -0300

    Add files via upload

---
+++ aula04.js
@@ -0,0 +1,13 @@
+let n1 = 9;
+let n2 = 9;
+
+let media = (n1 + n2) / 2;
+
+console.log(`Média ${media}`)
+
+if (media >= 9){
+    console.log(`Quadro de Honra`);
+}
+console.log(`Fim Programa`)
+
+let 
---
+++ exercicio.js
@@ -0,0 +1,44 @@
+
+// 1 - Criando Variáveis
+
+// Crie um programa que declare duas variáveis do tipo inteiro e exiba o resultado da 
+// soma dessas variáveis.
+
+var num1 = 5
+var num2 = 5
+
+let soma = num1 + num2 
+alert = (`(a soma dos numeros é ${soma}!`);
+
+// Crie um programa que declare uma variável do tipo string e exiba seu conteúdo na 
+// tela
+
+
+
+
+// Crie um programa que declare uma variável do tipo booleano e atribua a ela o valor 
+// "true". Em seguida, exiba o valor dessa variável na tela.
+
+var nota1 = 8.5;
+var nota2 = 10;
+var media = (nota1 + nota2) / 2 ;
+const SerAprovado = true
+console.log(`A media do aluno é: ${media}`);
+var mediaF = media > 7;
+console.log(`O aluno vai ser aprovado? ${SerAprovado}`);
+
+// Crie um programa que declare uma variável do tipo double e atribua a ela o valor 
+// 3.1415. Em seguida, exiba o valor dessa variável na tela.
+
+
+
+
+
+
+
+
+
+
+
+// 2 - Utilizando os conceitos apresentados, pergunte ao usuário.
+
