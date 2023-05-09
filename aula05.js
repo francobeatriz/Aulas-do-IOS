3f2ddd2c054dc61c4369f1f357ca49098eb452df
Beatriz Franco <129182039+francobeatriz@users.noreply.github.com>
2023-05-09 11:33:06 -0300

    Add files via upload

---
+++ aula05.js
@@ -0,0 +1,61 @@
+// Valores bonus = "ptsBonus3"
+
+// ptsBonus1 - console.log("2.0pts");
+// ptsBonus2 - console.log("1.5pts");
+// ptsBonus3 - console.log ("1.0pts");
+// ptsBonus4 - console.log ("0.5pts");
+// ptsBonus5 - console.log ("0.25pts");
+
+// let valorCompra = 1200;
+// // 32p < 1300
+// // 55p < 1500
+// // 75p > 2500
+// if (valorCompra <= 1300) {
+//     console.log('Vou comprar 32p');
+// } else if (valorCompra <= 1500) {
+//     console.log('Comprar 55p');
+// } else {
+//     console.log('Comprar de 75p');
+// }
+
+// console.log('Final do programa')
+
+
+let pontosAlunos = "ptsBonus1";
+
+switch (PontosAlunos) { 
+        case "ptsBonus1":
+        console.log("2.0pts");
+        break;
+
+        case "ptsBonus2":
+        console.log("1.5pts")
+        break
+
+        case "ptsBonus3":
+        console.log("1.0pts")
+        break
+
+        case "ptsBonus4":
+        console.log("0.5pts")
+        break
+
+        case "ptsBonus5":
+        console.log("0.25pts")
+        break
+
+        default:
+            console.log("Não foi aplicado pontos adicionais");
+            break;
+    }
+    console.log("Fim do programa");
+
+
+    // let idade =  14 // 15 - 16 - 17
+    // while (idade < 18) {
+    //     console.log (`Não pode beber ${idade}`)
+    //     idade += 1
+    // }
+    // console.log("Fim programa")
+
+    // let idade = 
--- exercicio.js
+++ exercicio.js
@@ -4,11 +4,11 @@
 // Crie um programa que declare duas variáveis do tipo inteiro e exiba o resultado da 
 // soma dessas variáveis.
 
-var num1 = 5
-var num2 = 5
+// var num1 = 5
+// var num2 = 5
 
-let soma = num1 + num2 
-alert = (`(a soma dos numeros é ${soma}!`);
+// let soma = num1 + num2 
+// alert = (`(a soma dos numeros é ${soma}!`);
 
 // Crie um programa que declare uma variável do tipo string e exiba seu conteúdo na 
 // tela
@@ -19,13 +19,13 @@ alert = (`(a soma dos numeros é ${soma}!`);
 // Crie um programa que declare uma variável do tipo booleano e atribua a ela o valor 
 // "true". Em seguida, exiba o valor dessa variável na tela.
 
-var nota1 = 8.5;
-var nota2 = 10;
-var media = (nota1 + nota2) / 2 ;
-const SerAprovado = true
-console.log(`A media do aluno é: ${media}`);
-var mediaF = media > 7;
-console.log(`O aluno vai ser aprovado? ${SerAprovado}`);
+// var nota1 = 8.5;
+// var nota2 = 10;
+// var media = (nota1 + nota2) / 2 ;
+// const SerAprovado = true
+// console.log(`A media do aluno é: ${media}`);
+// var mediaF = media > 7;
+// console.log(`O aluno vai ser aprovado? ${SerAprovado}`);
 
 // Crie um programa que declare uma variável do tipo double e atribua a ela o valor 
 // 3.1415. Em seguida, exiba o valor dessa variável na tela.
@@ -42,3 +42,84 @@ console.log(`O aluno vai ser aprovado? ${SerAprovado}`);
 
 // 2 - Utilizando os conceitos apresentados, pergunte ao usuário.
 
+// • Qual o nome do usuário; 
+// alert("Olá usuário!");
+// let nome = prompt ("Qual o seu nome?"); 
+// // • Qual sua idade; 
+// alert("Olá usuário!");
+// let nome = prompt ("Qual sua idade?"); 
+// // • Bairro onde mora; 
+// alert("Olá usuário!");
+// let nome = prompt ("Onde você mora?"); 
+
+// // • Passe o nome do usuário para todas as letras em maiúscula. 
+//  const s = 'Usuário';
+// console.log(s.toUpperCase());
+
+// • Crie um programa que solicite ao usuário que digite dois números inteiros e exiba o 
+// resultado da soma desses números na tela.
+// let num1 = Number(prompt('Digite um número '));
+// let num2 = Number(prompt('Digite outro número '));
+
+// let soma = num1 + num2; // Number ou parseFloat
+
+// alert(`A soma dos valores ${num1} + ${num2} é: ${soma}`);
+
+// 3- Aplicando metodos
+// Crie uma variável com o seu nome; 
+// • Depois utilizando o método Replace troque o seu nome do meio pelo último 
+// sobrenome; 
+// • Crie uma string para apresentar o resultado
+
+// let nome = '     Beatriz   '
+// let SobreNome = '     Franco      '
+
+// var nome2 = 
+// console.log(nome2)
+
+// var nome3 = "Beatriz Franco da Costa" .replace("Costa","Franco");
+// console.log(nome3);
+
+// Crie um variável preço e atribua a ela um valor numérico. Em seguida, crie um 
+// variável desconto e atribua a ela um valor 0.2, representando a porcentagem (20%) de 
+// desconto que você deseja aplicar. Por fim, crie uma variável Preço final que use as variáveis 
+// preço e desconto para calcular o preço com desconto e imprima o resultado no console
+
+// var preço = 10;
+// var desconto = 0.2;
+
+// let substring = preço - desconto
+
+// console.log (`O valor do produto é: ${substring}`)
+
+// Faça um programa conferir sua idade. Se a idade for menor do que 18 anos, exiba 
+// uma mensagem informando que o usuário é menor de idade. Se a idade for igual ou maior 
+// do que 18 anos e menor do que 60 anos, exiba uma mensagem informando que o usuário 
+// é adulto. Se a idade for igual ou maior do que 60 anos, exiba uma mensagem informando 
+// que o usuário é idoso. IF ELSE
+
+// let idade = 18;
+
+// if (idade >= 18) { 
+//     console.log('Menor de idade');
+// } else if (idade <= 60) {
+//     console.log('Adulto');
+// } else (idade >=60) {
+//     console.log('Idoso');
+// }
+
+// Faça um programa que teste a idade do usuário. Se a idade digitada for maior ou 
+// igual a 18, exiba a mensagem "Você pode dirigir". Caso contrário, exiba a mensagem "Você 
+// ainda não pode dirigir"
+
+// if (idade >= 18) {
+//     console.log("Você pode dirigir")
+// } else (idade <= 18) {
+//     console.log("Voce ainda não pode dirigir")
+// }
+
+
+// Faça um programa que peça ao usuário para digitar um número inteiro. Se o número 
+// for positivo, exiba uma mensagem informando que o número é positivo. Se o número for 
+// negativo, exiba uma mensagem informando que o número é negativo. Se o número for 
+// igual a zero, exiba uma mensagem informando que o número é zero. (Aplicar no Console)
