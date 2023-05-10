fbf35fcb66e130c034697087d3f78cd7f935d7a9
Beatriz Franco <129182039+francobeatriz@users.noreply.github.com>
2023-05-10 10:05:38 -0300

    Add files via upload

---
+++ aula06.js
@@ -0,0 +1,43 @@
+// let num = 0 ;
+// while (num <= 5) {
+//     console.log(`somando numeros`)
+// }
+
+// for(let num = 0; num <= 5 ;  num += 1) {
+//     console.log(`somando num: ${num}`)
+// }
+
+// ARRAY //
+
+// let SalaAula = ["Bia","Samuel","Adriana","Hilary",2]
+// console.log(SalaAula.length);
+// console.log(SalaAula[3]);
+
+// let nome = "Beatriz";
+// console.log(nome.length)
+
+// let mercado = [
+//     ["Pao","Arroz","Feijão"],
+//     ["Veja","Detergente","Sabão"],
+//     ["Chocolate","Danone","Biscoito"]
+// ];
+
+// console.log(mercado[0][2])
+
+// mercado[2][2] = "suco"
+// console.log(mercado[2][2]);
+
+// let numeros = [1,2,3,4,5]
+// console.log(numeros.join('-'));
+// console.log(typeof numeros);
+
+// let numeros2 = numeros.toString();
+// console.log(numeros2)
+// console.log(typeof numeros2)
+
+const frutas = ["Banana","Morango","Mamão","Abacaxi","Abacate"]
+console.log(frutas.length);
+
+for (let f = 0; f < frutas.length; f++) 
+console.log(`Eu gosto de: ${frutas[f]} \n`);
+
