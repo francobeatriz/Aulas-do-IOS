14ac1bc20c89d6d8a6a8d1afce7bbea4cb6c919f
Beatriz Franco <129182039+francobeatriz@users.noreply.github.com>
2023-05-16 08:33:43 -0300

    Add files via upload

---
+++ aula07.js
@@ -0,0 +1,22 @@
+// Arrays de Objetos
+
+let pessoa= {
+    nome: "Bia",
+    sobreNome: "Franco",
+    idade: 21,
+    regiao: Mooca,
+    corDosOlhos: verde,
+    hobbies: ['Esporte','Dormir','Ouvir musica'],
+    endereço: {
+        rua: 'Av. Palista',
+        cep: 5555,
+    },
+    salario: 4500,
+}
+
+console.log(pessoa.nome)
+console.log(pessoa.SobreNome)
+console.log(pessoa.idade)
+console.log(pessoa.endereco.rua,)
+
+
