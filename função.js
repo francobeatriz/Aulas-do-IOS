6b3f59381fdb35fa301605783f99229d9d3c4a18
Beatriz Franco <129182039+francobeatriz@users.noreply.github.com>
2023-05-23 09:11:01 -0300

    Add files via upload

---
+++ função.js
@@ -0,0 +1,12 @@
+//arrow function
+//function 
+
+const media = (n1, n2) => {
+    return (res = (n1 + n2) / 2);
+};
+
+function media2 (n1, n2) {
+    return (res = (n1 + n2) / 2);
+}
+
+console.log(media(10,5))
